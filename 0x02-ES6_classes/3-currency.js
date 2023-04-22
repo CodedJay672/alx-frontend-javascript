346d77bc239ef3fc651bd11f101d2e16c38c2b8f
codedjay672 <emmajoel6456@gmail.com>
2023-04-22 03:58:51 +0100

    fix currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -16,6 +16,6 @@ export default class Currency {
     return this._code;
   }
   displayFullCurrency() {
-    console.log(`${this._name} (${this._code})`);
+    return `${this._name} (${this._code})`;
   }
 }
