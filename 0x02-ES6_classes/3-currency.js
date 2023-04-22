acc79d20421abbf82abe664b0c92b34010a3f89a
codedjay672 <emmajoel6456@gmail.com>
2023-04-22 03:47:42 +0100

    currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,21 @@
+export default class Currency {
+  constructor (code, name) {
+    this._name = name;
+    this._code = code;
+  }
+  set name(arg) {
+    this._name = arg;
+  }
+  get name() {
+    return this._name;
+  }
+  set code(arg) {
+    this._code = arg;
+  }
+  get code() {
+    return this._code;
+  }
+  displayFullCurrency() {
+    console.log(`${this._name} (${this._code})`);
+  }
+}
