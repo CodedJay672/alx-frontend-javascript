d7f62569164fc03bda4ae8458c3e928a9e0ed080
codedjay672 <emmajoel6456@gmail.com>
2023-04-24 19:56:45 +0100

    add more tests

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(arg) {
-  if (typeof arg !== 'object') {
+  if (typeof arg !== 'object' || arg.length === 0) {
     return [];
   }
   return arg.map(({ id }) => id);
