33d91d327e6160b510609f64ca328270c4597f32
codedjay672 <emmajoel6456@gmail.com>
2023-04-25 23:20:47 +0100

    set and arrays

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  const result = [];
+  set.forEach((str) => {
+    if (str.startsWith(startString)) {
+      result.push(str.slice(startString.length));
+    }
+  });
+  return result.join('-');
+}
