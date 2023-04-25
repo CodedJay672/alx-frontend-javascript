9b61d8696a7678a019aa9193ca718194f41e8f2b
codedjay672 <emmajoel6456@gmail.com>
2023-04-25 23:43:56 +0100

    mapping

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const arrayObject = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  const map = new Map(arrayObject);
+  return map;
+}
