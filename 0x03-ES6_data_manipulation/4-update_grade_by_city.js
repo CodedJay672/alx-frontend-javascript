1ee64b8a2e188e14c1f45d16bb8084e68a5ae76a
codedjay672 <emmajoel6456@gmail.com>
2023-04-28 16:47:39 +0100

    eslint fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,13 +3,11 @@ export default function updateStudentGradeByCity(arg, city, newGrades) {
     const myObj = obj;
     for (let x = 0; x < newGrades.length; x += 1) {
       if (myObj.id === newGrades[x].studentId) {
-	myObj.grade = newGrades[x].grade;
-	break;
+        myObj.grade = newGrades[x].grade;
+        break;
       } else if (x === newGrades.length - 1) {
         myObj.grade = 'N/A';
-	break;
-      } else {
-        continue;
+        break;
       }
     }
     return myObj;
